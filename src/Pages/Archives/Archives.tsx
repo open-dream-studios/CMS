9fc24acbca6fead5f77a267b8f2b5cc7267867cf
JosephGoff <joeygoff13@gmail.com>
2024-11-28 10:55:56 -0500

    thursday nov 28 11am

--- src/Pages/Archives/Archives.tsx
+++ src/Pages/Archives/Archives.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { PageProps } from '../../App';
+import OliveDisplay from '../../Components/Navbar/OliveDisplay/OliveDisplay';
 
 const Archives: React.FC<PageProps> = ({ navigate }) => (
   <div
+    className="w-[100%] min-h-[100vh]"
     style={{
       width: "100%",
-      height: "2000px",
       backgroundColor: "pink",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
     }}
   >
-    <h1>Archives Page</h1>
+    <h1>
+      {/* <OliveDisplay /> */}
+    </h1>
   </div>
 );
 
